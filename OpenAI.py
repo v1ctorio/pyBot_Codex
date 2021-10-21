f22e917d0fa3304223b0e6f00f4b7966c4f074ce
jorgegzz <A01571510@itesm.mx>
2021-10-21 10:51:48 -0500

    command help added

--- OpenAI.py
+++ OpenAI.py
@@ -1,38 +1,61 @@
 import openai
 
+
 def explain(code):
     prompt = "# Python 3\n" \
              f"{code}\n\n" \
              "\"\"\"\n" \
              "Here is what the code above is doing:\n" \
-             "1."
+
     response = openai.Completion.create(
-      engine="davinci-codex",
-      prompt=prompt,
-      temperature=0,
-      max_tokens=64,
-      top_p=1.0,
-      frequency_penalty=0.0,
-      presence_penalty=0.0,
-      stop=["\"\"\""]
+        engine="davinci-codex",
+        prompt=prompt,
+        temperature=0,
+        max_tokens=64,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        stop=["\"\"\""]
     )
     story = response['choices'][0]['text']
     print(prompt + story)
     return str(story)
+
 
 def code(instructions):
     prompt = '"""\n' \
              f'{instructions}' \
              '"""'
     response = openai.Completion.create(
-      engine="davinci-codex",
-      prompt=prompt,
-      temperature=0,
-      max_tokens=300,
-      top_p=1.0,
-      frequency_penalty=0.0,
-      presence_penalty=0.0,
-      stop=['"""']
+        engine="davinci-codex",
+        prompt=prompt,
+        temperature=0,
+        max_tokens=300,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        stop=['"""']
+    )
+    story = response['choices'][0]['text']
+    print(prompt + story)
+    return str(story)
+
+
+def ask(question):
+    prompt = "Python chatbot\n\n\n" \
+             "You: How do I combine lists?\n" \
+             "Python chatbot: You can use the `extend()` method.\n" \
+             f"You: {question}\n" \
+             "Python chatbot:"
+    response = openai.Completion.create(
+        engine="davinci-codex",
+        prompt=prompt,
+        temperature=0,
+        max_tokens=60,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        stop=['You:']
     )
     story = response['choices'][0]['text']
     print(prompt + story)
--- main.py
+++ main.py
@@ -25,17 +25,71 @@ async def on_ready():
 
 
 @client.command()
+async def help(ctx):
+    embed = discord.Embed(
+        title="List of commands",
+        color=discord.Colour.from_rgb(225, 224, 120)
+
+    )
+    embed.add_field(
+        name="**py ask**",
+        value="Ask any python related question.\n" \
+              "Ex. `py ask How do I use regex?`",
+        inline=False
+    )
+    embed.add_field(
+        name="**py code**",
+        value="pyBot will code following your instructions\n" \
+              "Ex. `py code\n1. import random\n2. Print a random number between 1 and 100`",
+        inline=False
+    )
+    explain_example_code = \
+        r"""word = input()
+output = ""
+first_vowel = True
+for char in word:
+    if char in "aeiou" and first_vowel:
+        char *= 10
+        first_vowel = False
+    output += char
+
+print(output)
+"""
+    embed.add_field(
+        name="**ger explain**",
+        value="pyBot will explain the piece of code you ask\n" \
+              f"Ex.`py explain` {await pyify(explain_example_code)}",
+        inline=False
+    )
+    embed.set_footer(
+        text="Warning: this bot is still being developed and you may encounter errors"
+    )
+    emoji = "\u2705"
+    await ctx.message.add_reaction(emoji)
+    await ctx.author.send(embed=embed)
+
+
+@client.command()
 async def explain(ctx, *, arg):
-    code = await depyify(arg)
-    explanation = OpenAI.explain(code)
-    await ctx.send(f"`1.{explanation}`")
+    async with ctx.typing():
+        code = await depyify(arg)
+        explanation = OpenAI.explain(code)
+        await ctx.send(f"**Here is what the code above is doing:**`{explanation}`")
 
 
 @client.command()
 async def code(ctx, *, arg):
-    code = OpenAI.code(arg)
-    code = await pyify(code)
-    await ctx.send(f"{code}")
+    async with ctx.typing():
+        code = OpenAI.code(arg)
+        code = await pyify(code)
+        await ctx.send(f"**You can do that by following the code below:**\n{code}")
+
+
+@client.command()
+async def ask(ctx, *, question):
+    async with ctx.typing():
+        answer = OpenAI.ask(question)
+        await ctx.send(f"{answer}")
 
 
 if __name__ == "__main__":
